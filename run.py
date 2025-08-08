baaef751fbccde5ba2200786139cdaa34fdb0753
Alex Alagoa Biobelemo <67587506+AlexBiobelemo@users.noreply.github.com>
2025-08-08 14:14:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ app = create_app()
 if __name__ == '__main__':
     # Setting debug=True enables auto-reloading and shows detailed errors in the browser.
     # Turn this off for production.
-    app.run(debug=True)
+
+    app.run(debug=False)
